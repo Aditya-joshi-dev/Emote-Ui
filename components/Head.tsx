1795f6b8db75e94e59a7ac9fc259e995e23d60cf
Aditya-joshi-dev <aditya.joshi0666@gmail.com>
2024-07-20 12:23:14 +0545

    modified Head components

--- components/Head.tsx
+++ components/Head.tsx
@@ -5,21 +5,26 @@ const Head = () => {
   return (
     <View style={styles.main}>
       <View>
-        <Image 
-        source={{uri:'https://imgs.search.brave.com/pZ2DKWjtw7hzsB-caM9l5n5xAr6aaH4tXxJAIMSHK5s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0Lzk4LzcyLzQz/LzM2MF9GXzQ5ODcy/NDMyM19Gb25BeThM/WVlmRDFCVUMwYmNL/NTZhb1l3dUxISjJH/ZS5qcGc'}} 
-        style={styles.profileImg}/>
-        
+        <Image
+          source={{
+            uri: 'https://imgs.search.brave.com/pZ2DKWjtw7hzsB-caM9l5n5xAr6aaH4tXxJAIMSHK5s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0Lzk4LzcyLzQz/LzM2MF9GXzQ5ODcy/NDMyM19Gb25BeThM/WVlmRDFCVUMwYmNL/NTZhb1l3dUxISjJH/ZS5qcGc',
+          }}
+          style={styles.profileImg}
+        />
       </View>
 
       <View style={styles.midContainer}>
         <Text style={styles.headtitle}>Hello, User</Text>
         <Text style={styles.subTitle}>Today, Date</Text>
       </View>
-      
+
       <View>
-         <Image 
-         source={{uri:'https://imgs.search.brave.com/cDNQQDbsID5Giok5WMyHvvwgRLZH9EXMlhbRvWHftHI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzA5LzQ2Lzc3/LzM2MF9GXzEwOTQ2/Nzc4NV9lZVlXSDJ0/WTRDbmtEbDlCdHVZ/TzZoV2prN2hIMG9r/VS5qcGc'}} 
-         style={styles.searchIcon}/>
+        <Image
+          source={{
+            uri: 'https://imgs.search.brave.com/cDNQQDbsID5Giok5WMyHvvwgRLZH9EXMlhbRvWHftHI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzA5LzQ2Lzc3/LzM2MF9GXzEwOTQ2/Nzc4NV9lZVlXSDJ0/WTRDbmtEbDlCdHVZ/TzZoV2prN2hIMG9r/VS5qcGc',
+          }}
+          style={styles.searchIcon}
+        />
       </View>
     </View>
   );
@@ -28,35 +33,31 @@ const Head = () => {
 export default Head;
 
 const styles = StyleSheet.create({
-    main:{
-        flex:1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderRadius: 10,
-        margin: 10,
+  main: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 10,
+  },
+  profileImg: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+  },
 
-    },
-    profileImg:{
-        height: 50,
-        width: 50,
-        borderRadius: 50/2,
-    },
-  
-    midContainer:{
-       
-    },
-    headtitle:{
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
-    subTitle:{
-        paddingLeft: 12,
-    },
-    searchIcon:{
-       height:35,
-       width:35,
-       borderRadius: 60/2,
-
-    }
+  midContainer: {},
+  headtitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  subTitle: {
+    paddingLeft: 12,
+  },
+  searchIcon: {
+    height: 35,
+    width: 35,
+    borderRadius: 60 / 2,
+  },
 });
