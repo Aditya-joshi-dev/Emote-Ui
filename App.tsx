eb3c3ddb990324434aa473284102ccf43fecb9ba
Aditya-joshi-dev <aditya.joshi0666@gmail.com>
2024-07-20 12:23:50 +0545

    added all the ui components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Head from './components/Head'
-import MainCard from './components/MainCard'
+import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import React from 'react';
+import Head from './components/Head';
+import MainCard from './components/MainCard';
+import PlanBody from './components/PlanBody';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.mainColr }>
       <ScrollView>
         <Head />
-        <MainCard/>
+        <MainCard />
+        <PlanBody />
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainColr:{
+    backgroundColor: '#F2E2D2'
+},
+});
